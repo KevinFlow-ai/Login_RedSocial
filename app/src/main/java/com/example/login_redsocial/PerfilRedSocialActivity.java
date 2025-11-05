2884bbf74e5e51593c5e3075f595d14bb993b1eb
KevinFlow-ai <cosoyeray@gmail.com>
2025-11-05 17:58:08 +0100

    Hacer una aplicacion de una red social con distintas interfaces como  es el login, perfil de usuario y home.

---
+++ app/src/main/java/com/example/login_redsocial/PerfilRedSocialActivity.java
@@ -0,0 +1,14 @@
+package com.example.login_redsocial;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+// CLASE PARA MOSTRAR EL PERFIL SOCIAL
+public class PerfilRedSocialActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.perfil_red_social); // Usa tu layout del perfil
+    }
+}
+
