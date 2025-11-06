7fa5564df413e2de78dad9c9fdd5a0c50265b509
KevinFlow-ai <cosoyeray@gmail.com>
2025-11-06 01:54:16 +0100

    se implento la posicion horizontal_land a la nueva interfaz de spotify.

---
+++ app/src/main/java/com/example/login_redsocial/SpotyActivity.java
@@ -0,0 +1,12 @@
+package com.example.login_redsocial;  // Usa el paquete correcto de tu app
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SpotyActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spoty);  // Carga el layout activity_spoty.xml
+    }
+}
