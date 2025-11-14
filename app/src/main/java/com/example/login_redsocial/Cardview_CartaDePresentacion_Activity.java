260ca0194c1017392e95073d01a573e7c3ff359f
KevinFlow-ai <cosoyeray@gmail.com>
2025-11-14 20:35:35 +0100

    se creo el layaut y la clase java del cardview

---
+++ app/src/main/java/com/example/login_redsocial/Cardview_CartaDePresentacion_Activity.java
@@ -0,0 +1,16 @@
+package com.example.login_redsocial;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Cardview_CartaDePresentacion_Activity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cardview_carta_presentacion);
+
+    }
+}
