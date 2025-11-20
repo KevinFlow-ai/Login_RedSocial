51170345bb909c4b7295201cc2561e9bdf15aa0a
KevinFlow-ai <cosoyeray@gmail.com>
2025-11-20 14:24:43 +0100

    practicas para el examen. como poner una foto redonda y como lanzar un layaout

---
+++ app/src/main/java/com/example/login_redsocial/practicaExamen.java
@@ -0,0 +1,14 @@
+package com.example.login_redsocial;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+// CLASE PARA MOSTRAR EL PERFIL SOCIAL
+public class practicaExamen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.practica_examen); // Usa tu layout del perfil
+    }
+}
+
