b4b25e11398b0f8d92becd2c46240ae961c1beea
KevinFlow-ai <cosoyeray@gmail.com>
2025-11-27 20:57:39 +0100

    Se probó a ocultar el status bar en la pantalla del perfil.

--- app/src/main/java/com/example/login_redsocial/PerfilRedSocialActivity.java
+++ app/src/main/java/com/example/login_redsocial/PerfilRedSocialActivity.java
@@ -1,6 +1,8 @@
 package com.example.login_redsocial;
 
 import android.os.Bundle;
+import android.view.WindowInsets;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 // CLASE PARA MOSTRAR EL PERFIL SOCIAL
@@ -9,6 +11,10 @@ public class PerfilRedSocialActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.perfil_red_social); // Usa tu layout del perfil
+        //  esto oculta todo el status bar
+        //getWindow().getInsetsController().hide(WindowInsets.Type.statusBars());
+
+
     }
 }
 
